534a35d393b4e29d7019a933a56103ea00e1fc90
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-05-05 09:09:21 -0700

    JS lesson-1

---
+++ app.js
@@ -0,0 +1,19 @@
+let userName = prompt('Enter your name');
+let age = +prompt('Enter your age');
+let country = prompt('where are you from?');
+const dev = confirm('Are you a developper?');
+const stu = confirm('Are you a student?')
+
+const object = {
+    name: `Name: ${userName}`,
+    age : `Age: ${age}`,
+    country: `Country: ${country}`,
+    dev: `Developper: ${dev}`,
+    stu: `Student: ${stu}`
+ } 
+
+console.log(object.name);
+console.log(object.age);
+console.log(object.country);
+console.log(object.dev);
+console.log(object.stu);
