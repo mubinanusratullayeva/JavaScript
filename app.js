815837cd29b85a63b2fcb1f28b7ee89e8711ef7b
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-05-11 12:52:49 -0700

    entered codes js-3

--- app.js
+++ app.js
@@ -54,3 +54,39 @@
 // }
 
 
+
+
+
+
+
+
+
+
+//JS LESSON-3
+
+const foo = document.querySelector('.val'),
+       num = document.querySelector('.num'),
+       cur = document.querySelector('.cur'),
+       usd = document.querySelector('.usd'),
+       rubl = document.querySelector('.rubl'),
+       euro = document.querySelector('.euro'),
+       bar = document.querySelector('.bar');
+
+
+foo.addEventListener('submit', (event) =>  {
+    event.preventDefault()
+
+    let inp = +num.value
+
+   if (cur.value === usd.value) {
+    let dol = inp * 11465;
+       bar.textContent = `${dol} sum`
+   }else if (cur.value === rubl.value) {
+    let rub = inp * 149.13
+    bar.textContent = `${rub} sum`
+   } else {
+    let yev = inp * 12354.03
+    bar.textContent = `${yev} sum`
+   }
+
+})
