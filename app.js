83270c5e4876322a8ce0d506768f2c1d869214ed
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-05-11 09:25:13 -0700

    html structure

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-let userName = prompt('Enter your name');
-let age = +prompt('Enter your age');
-let country = prompt('where are you from?');
-const dev = confirm('Are you a developper?');
-const stu = confirm('Are you a student?')
+'use strict';
 
-const object = {
-    name: `Name: ${userName}`,
-    age : `Age: ${age}`,
-    country: `Country: ${country}`,
-    dev: `Developper: ${dev}`,
-    stu: `Student: ${stu}`
- } 
+//JS LESSON-1
 
-console.log(object.name);
-console.log(object.age);
-console.log(object.country);
-console.log(object.dev);
-console.log(object.stu);
+// let userName = prompt('Enter your name');
+// let age = +prompt('Enter your age');
+// let country = prompt('where are you from?');
+// const dev = confirm('Are you a developper?');
+// const stu = confirm('Are you a student?')
+
+// const object = {
+//     name: `Name: ${userName}`,
+//     age : `Age: ${age}`,
+//     country: `Country: ${country}`,
+//     dev: `Developper: ${dev}`,
+//     stu: `Student: ${stu}`
+//  } 
+
+// console.log(object.name);
+// console.log(object.age);
+// console.log(object.country);
+// console.log(object.dev);
+// console.log(object.stu);
+
+
+
+
+
+//JS LESSON-3
+
